a17e63893156308019065f3dd83fc17080a6f2e4
Khoa Huynh <khoawin92@gmail.com>
2018-11-09 11:39:33 -0800

    added responsivenesss to photos page

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -24,58 +24,85 @@ import tunnel1 from '../assets/tunnel1.jpg';
 
 
 
-const styles = {
-  container: {
-    width: '90vw',
-    display: 'flex',
-    margin: 'auto',
-    flexDirection: 'column',
-    justifyContent: 'center',
-  },
-  wideImage: {
-    marginTop: 10,
-    marginBottom: 10,
-    width: '100%',
-    height: '40vh',
-    maxHeight: 300,
-  },
-  smallImage: {
-    marginTop: 10,
-    marginBottom: 10,
-    width: '38%',
-    maxHeight: 300,
-    height: '40vh',
-  },
-  middleImage: {
-    marginTop: 10,
-    marginBottom: 10,
-    maxHeight: 300,
-    width: '38%',
-    marginLeft: '1%',
-    marginRight: '1%',
-    height: '40vh',
-  },
-  threeRow: {
-    width: '100%',
-    display: 'flex',
-    margin: 'auto',
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
-  image: {
-    width: '100%',
-    height: '100%',
-    objectFit: 'cover',
-  },
-  introText: {
-    width: '80%',
-    margin: 'auto',
-    marginTop: '5vh',
-    marginBottom: '5vh',
-    fontSize: 14,
-    textAlign: 'center',
-    color: '#808080',
-  },
+const styles = theme => {
+
+  theme.breakpoints.values.sm = 650;
+  theme.breakpoints.values.md = 974;
+  theme.breakpoints.values.lg = 1024;
+  theme.breakpoints.values.imageBreakMd = 974;
+  theme.breakpoints.values.imageBreakSm = 650;
+
+  return {
+    container: {
+      width: '90vw',
+      // display: 'flex',
+      margin: 'auto',
+      // flexDirection: 'column',
+      // justifyContent: 'center',
+    },
+    wideImage: {
+      marginTop: 10,
+      marginBottom: 10,
+      width: '100%',
+      height: '40vh',
+      maxHeight: 300,
+      minHeight: 270,
+    },
+    smallImage: {
+      [theme.breakpoints.between('xs', 'sm')]: {
+        width: '100%',
+      },
+      [theme.breakpoints.between('sm', 'md')]: {
+        width: '48%',
+        marginRight: '1%',
+        marginLeft: '1%',
+      },
+      display: 'inline-block',
+      marginTop: 10,
+      marginBottom: 10,
+      width: '32%',
+      maxHeight: 300,
+      minHeight: 270,
+      minWidth: 280,
+      height: '40vh',
+    },
+    middleImage: {
+      [theme.breakpoints.between('xs', 'sm')]: {
+        width: '100%',
+        marginLeft: '0%',
+        marginRight: '0%',
+      },
+      [theme.breakpoints.between('sm', 'md')]: {
+        width: '48%',
+        marginRight: '1%',
+        marginLeft: '1%',
+      },
+      display: 'inline-block',
+      marginTop: 10,
+      marginBottom: 10,
+      maxHeight: 300,
+      minHeight: 270,
+      minWidth: 280,
+      width: '32%',
+      marginLeft: '2%',
+      marginRight: '2%',
+      height: '40vh',
+    },
+    image: {
+      width: '100%',
+      height: '100%',
+      objectFit: 'cover',
+    },
+    introText: {
+      width: '80%',
+      margin: 'auto',
+      marginTop: '5vh',
+      marginBottom: '5vh',
+      fontSize: 14,
+      textAlign: 'center',
+      color: '#808080',
+    },
+  }
 };
 
 class Photos extends Component {
@@ -90,7 +117,6 @@ class Photos extends Component {
               <img src={fall} className={classes.image} />
             </Card>
 
-            <div className={classes.threeRow}>
               <Card className={classes.smallImage} elevation={8}>
                 <img src={heatherMaple} className={classes.image} />
               </Card>
@@ -100,9 +126,8 @@ class Photos extends Component {
               <Card className={classes.smallImage} elevation={8}>
                 <img src={bandera} className={classes.image} />
               </Card>
-            </div>
+        
 
-            <div className={classes.threeRow}>
               <Card className={classes.smallImage} elevation={8}>
                 <img src={highRock2} className={classes.image} />
               </Card>
@@ -112,9 +137,7 @@ class Photos extends Component {
               <Card className={classes.smallImage} elevation={8}>
                 <img src={highRock1} className={classes.image} />
               </Card>
-            </div>
 
-            <div className={classes.threeRow}>
               <Card className={classes.smallImage} elevation={8}>
                 <img src={rattle1} className={classes.image} />
               </Card>
@@ -124,9 +147,7 @@ class Photos extends Component {
               <Card className={classes.smallImage} elevation={8}>
                 <img src={rattle2} className={classes.image} />
               </Card>
-            </div>
 
-            <div className={classes.threeRow}>
               <Card className={classes.smallImage} elevation={8}>
                 <img src={snowlake1} className={classes.image} />
               </Card>
@@ -136,7 +157,6 @@ class Photos extends Component {
               <Card className={classes.smallImage} elevation={8}>
                 <img src={snowlake2} className={classes.image} />
               </Card>
-            </div>
 
 
           </Grid>
