94720a3138dd226d5fd1942c695ef3bb317b4764
Khoa Huynh <khoawin92@gmail.com>
2018-11-09 19:00:07 -0800

    working solution for github pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Dashboard from './Dashboard.js';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import Navbar from './Navbar.js';
 import About from './About.js';
 import Photos from './Photos.js';
@@ -10,7 +10,7 @@ import Projects from './Projects.js';
 export default class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <HashRouter>
         <React.Fragment>
           <Navbar />
           <Route exact path='/' component={Dashboard} />
@@ -20,7 +20,7 @@ export default class App extends Component {
           <Route path='/photos' component={Photos}/>
 
         </React.Fragment>
-      </BrowserRouter>
+      </HashRouter>
     );
   }
 }
